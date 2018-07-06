0b05cd65b461ad7455760d64c200d6604e477b29
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2018-07-06 03:53:06 +0100

    - `onHotspotOver`: sets the currentTime of vid to take snapshot. and displays modal Problem: (video is not suppose to change position during onHotspotOver) - `onHotspotLeave` remove modal

--- src/Video.js
+++ src/Video.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import fancyTimeFormat from './utils';
 
 const Hospot = (props) => {
     return (
-        <svg
-            id="icon-star"
-            viewBox="0 0 26 28"
-            width="15"
-            height="15"
-            className="hotspot"
-            style={{ left: `${props.left}% ` }}
-            onClick={() => props.onClick(props.left)}>
-            <title>star</title>
-            <path d="M26 10.109c0 0.281-0.203 0.547-0.406 0.75l-5.672 5.531 1.344 7.812c0.016 0.109 0.016 0.203 0.016 0.313 0 0.406-0.187 0.781-0.641 0.781-0.219 0-0.438-0.078-0.625-0.187l-7.016-3.687-7.016 3.687c-0.203 0.109-0.406 0.187-0.625 0.187-0.453 0-0.656-0.375-0.656-0.781 0-0.109 0.016-0.203 0.031-0.313l1.344-7.812-5.688-5.531c-0.187-0.203-0.391-0.469-0.391-0.75 0-0.469 0.484-0.656 0.875-0.719l7.844-1.141 3.516-7.109c0.141-0.297 0.406-0.641 0.766-0.641s0.625 0.344 0.766 0.641l3.516 7.109 7.844 1.141c0.375 0.063 0.875 0.25 0.875 0.719z"></path>
-        </svg>
+        <a href={`#${props.hotspotDuration}`}>
+            <svg
+                id="icon-star"
+                viewBox="0 0 26 28"
+                width="15"
+                height="15"
+                className="hotspot"
+                style={{ left: `${props.left}% ` }}
+                onClick={() => props.onClick(props.left)}
+                onMouseEnter={() => props.onMouseOver(props.left)}
+                onMouseLeave={() => props.onMouseLeave(props.left)}
+            >
+                <title>star</title>
+                <path d="M26 10.109c0 0.281-0.203 0.547-0.406 0.75l-5.672 5.531 1.344 7.812c0.016 0.109 0.016 0.203 0.016 0.313 0 0.406-0.187 0.781-0.641 0.781-0.219 0-0.438-0.078-0.625-0.187l-7.016-3.687-7.016 3.687c-0.203 0.109-0.406 0.187-0.625 0.187-0.453 0-0.656-0.375-0.656-0.781 0-0.109 0.016-0.203 0.031-0.313l1.344-7.812-5.688-5.531c-0.187-0.203-0.391-0.469-0.391-0.75 0-0.469 0.484-0.656 0.875-0.719l7.844-1.141 3.516-7.109c0.141-0.297 0.406-0.641 0.766-0.641s0.625 0.344 0.766 0.641l3.516 7.109 7.844 1.141c0.375 0.063 0.875 0.25 0.875 0.719z"></path>
+            </svg>
+        </a>
     )
 }
 class Video extends Component {
@@ -25,7 +31,11 @@ class Video extends Component {
             duration: 0,
             progress: 0,
             videoWidth: 0,
-            videoHeight: 0
+            videoHeight: 0,
+            modalLocation: 0,
+            modal: "None",
+            imgSrc: "",
+            hotspotDuration: "",
         }
     }
 
@@ -50,13 +60,32 @@ class Video extends Component {
         ctx.drawImage(this.vid, 0, 0, this.vid.videoWidth, this.vid.videoHeight);
         var img = new Image();
         img.src = this.canvas.toDataURL("image/png");
+        this.setState({
+            imgSrc: this.canvas.toDataURL("image/png"),
+            modal: ""
+        })
         img.width = this.vid.videoWidth;
-        this.ssContainer.appendChild(img);
     }
 
     onHotspotClick = (percent) => {
         const currentTime = this.state.duration * (percent / 100)
         this.vid.currentTime = currentTime;
+    }
+
+    onHotspotOver = (percent) => {
+        const currentTime = this.state.duration * (percent / 100)
+        this.vid.currentTime = currentTime;
+
+        this.setState({
+            modalLocation: percent,
+            hotspotDuration: fancyTimeFormat(currentTime).split(".")[0]
+        })
+    }
+
+    onHotspotLeave = () => {
+        this.setState({
+            modal: "None"
+        })
     }
 
 
@@ -76,13 +105,47 @@ class Video extends Component {
                 </video>
 
                 <div className="storyLine">
+                    <span className={`modal ${this.state.modal}`} style={{ left: `${this.state.modalLocation}%` }}>
+                        <img src={this.state.imgSrc} className="imgSource" alt="hospot" />
+                        <p>Time: {this.state.hotspotDuration} Minutes</p>
+                    </span>
                     <span className="progressColor" style={{ width: `${this.state.progress}%` }}></span>
                     <span className="progress" style={{ left: `${this.state.progress}%` }}></span>
-                    <Hospot left={10} onClick={(n) => this.onHotspotClick(n)} />
-                    <Hospot left={30} onClick={(n) => this.onHotspotClick(n)} />
-                    <Hospot left={50} onClick={(n) => this.onHotspotClick(n)} />
-                    <Hospot left={70} onClick={(n) => this.onHotspotClick(n)} />
-                    <Hospot left={90} onClick={(n) => this.onHotspotClick(n)} />
+                    <Hospot
+                        left={10}
+                        onClick={(n) => this.onHotspotClick(n)}
+                        onMouseOver={(n) => this.onHotspotOver(n)}
+                        onMouseLeave={(n) => this.onHotspotLeave(n)}
+                        hotspotDuration={this.state.hotspotDuration}
+                    />
+                    <Hospot
+                        left={30}
+                        onClick={(n) => this.onHotspotClick(n)}
+                        onMouseOver={(n) => this.onHotspotOver(n)}
+                        onMouseLeave={(n) => this.onHotspotLeave(n)}
+                        hotspotDuration={this.state.hotspotDuration}
+                    />
+                    <Hospot
+                        left={50}
+                        onClick={(n) => this.onHotspotClick(n)}
+                        onMouseOver={(n) => this.onHotspotOver(n)}
+                        onMouseLeave={(n) => this.onHotspotLeave(n)}
+                        hotspotDuration={this.state.hotspotDuration}
+                    />
+                    <Hospot
+                        left={70}
+                        onClick={(n) => this.onHotspotClick(n)}
+                        onMouseOver={(n) => this.onHotspotOver(n)}
+                        onMouseLeave={(n) => this.onHotspotLeave(n)}
+                        hotspotDuration={this.state.hotspotDuration}
+                    />
+                    <Hospot
+                        left={90}
+                        onClick={(n) => this.onHotspotClick(n)}
+                        onMouseOver={(n) => this.onHotspotOver(n)}
+                        onMouseLeave={(n) => this.onHotspotLeave(n)}
+                        hotspotDuration={this.state.hotspotDuration}
+                    />
                 </div>
 
                 <canvas
@@ -92,7 +155,6 @@ class Video extends Component {
                     className="canvas"
                 >
                 </canvas>
-                <div ref={el => this.ssContainer = el}></div>
             </div>
         )
     }
