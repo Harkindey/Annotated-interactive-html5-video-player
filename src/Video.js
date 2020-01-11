2422659e4d8dfb3029e4d47e30e78c90f777584f
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2020-01-11 15:10:30 +0100

    Successfully changed to useReducer hooks but bug

--- src/Video.js
+++ src/Video.js
@@ -1,8 +1,8 @@
-import React, { Component, useReducer } from 'react';
+import React, { useReducer } from 'react';
 import fancyTimeFormat from './utils';
 import { Hotspot } from './Component/hotspot';
 
-const intialState = {
+const initialState = {
 	duration: 0,
 	progress: 0,
 	videoWidth: 0,
@@ -16,258 +16,278 @@ const intialState = {
 	visibility: '',
 };
 
-reducer = (state, action) => {
+const reducer = (state, action) => {
 	switch (action.type) {
-		case 'UPDATE_PROGRESS':
+		case 'UPDATE_STATE':
+			return { ...state, ...action.payload };
+		default:
 			return { ...state, ...action.payload };
 	}
 };
 
-class Video extends Component {
-	constructor(props) {
-		super(props);
-		this.vid = null;
-		this.canvas = null;
-		this.ssContainer = null;
-		this.noneVid = null;
-		this.state = {
-			duration: 0,
-			progress: 0,
-			videoWidth: 0,
-			videoHeight: 0,
-			modalLocation: 0,
-			modal: 'None',
-			imgSrc: '',
-			hotspotDuration: '',
-			currentplayer: 'vid2',
-			playing: false,
-			visibility: '',
-		};
-	}
+const Video = props => {
+	const [state, dispatch] = useReducer(reducer, initialState);
 
-	onLoadedMetadata = () => {
-		this.setState({
-			duration: this.noneVid.duration,
-			videoWidth: this.noneVid.videoWidth,
-			videoHeight: this.noneVid.videoHeight,
+	let canvas = null;
+	let noneVid = null;
+	let vid1 = null;
+	let vid2 = null;
+
+	const onLoadedMetadata = () => {
+		dispatch({
+			type: 'UPDATE_STATE',
+			payload: {
+				duration: noneVid.duration,
+				videoWidth: noneVid.videoWidth,
+				videoHeight: noneVid.videoHeight,
+			},
 		});
 	};
 
-	onTimeUpdate = () => {
-		const { duration } = this.state;
-		if (this.state.currentplayer === 'vid1') {
-			const progress = (this.vid1.currentTime / duration) * 100;
-			this.setState({
-				progress,
+	const onTimeUpdate = () => {
+		const { duration } = state;
+		if (state.currentplayer === 'vid1') {
+			const progress = (vid1.currentTime / duration) * 100;
+			dispatch({
+				type: 'UPDATE_STATE',
+				payload: {
+					progress,
+				},
 			});
 		}
 
-		if (this.state.currentplayer === 'vid2') {
-			const progress = (this.vid2.currentTime / duration) * 100;
-			this.setState({
-				progress,
+		if (state.currentplayer === 'vid2') {
+			const progress = (vid2.currentTime / duration) * 100;
+			dispatch({
+				type: 'UPDATE_STATE',
+				payload: {
+					progress,
+				},
 			});
 		}
 	};
 
-	onHotspotClick = percent => {
-		const currentTime = this.state.duration * (percent / 100);
-		if (!this.state.playing) {
-			if (this.state.currentplayer === 'vid1')
-				this.vid1.currentTime = currentTime;
+	const onHotspotClick = percent => {
+		const currentTime = state.duration * (percent / 100);
+		if (!state.playing) {
+			if (state.currentplayer === 'vid1') vid1.currentTime = currentTime;
 
-			if (this.state.currentplayer === 'vid2')
-				this.vid2.currentTime = currentTime;
+			if (state.currentplayer === 'vid2') vid2.currentTime = currentTime;
 		} else {
-			if (this.state.currentplayer === 'vid1') {
-				this.setState({
-					currentplayer: 'vid2',
-					visibility: '',
+			if (state.currentplayer === 'vid1') {
+				dispatch({
+					type: 'UPDATE_STATE',
+					payload: {
+						currentplayer: 'vid2',
+						visibility: '',
+					},
 				});
-				this.vid2.currentTime = currentTime;
-				this.vid2.play();
+				vid2.currentTime = currentTime;
+				vid2.play();
 				setTimeout(() => {
-					this.vid1.load();
-					// this.vid1.currentTime = 0;
+					vid1.load();
+					// vid1.currentTime = 0;
 				}, 2000);
 			}
 
-			if (this.state.currentplayer === 'vid2') {
-				this.setState({
-					currentplayer: 'vid1',
-					visibility: 'opaque',
+			if (state.currentplayer === 'vid2') {
+				dispatch({
+					type: 'UPDATE_STATE',
+					payload: {
+						currentplayer: 'vid1',
+						visibility: 'opaque',
+					},
 				});
-				this.vid1.currentTime = currentTime;
-				this.vid1.play();
+				vid1.currentTime = currentTime;
+				vid1.play();
 				setTimeout(() => {
-					this.vid2.load();
-					//this.vid2.currentTime = 0;
+					vid2.load();
+					//vid2.currentTime = 0;
 				}, 3000);
 			}
 		}
 	};
 
-	onTimeUpdateForScreenshot = () => {
-		this.grabScreenshot();
+	const onTimeUpdateForScreenshot = () => {
+		grabScreenshot();
 	};
 
-	grabScreenshot = () => {
-		let ctx = this.canvas.getContext('2d');
-		ctx.drawImage(
-			this.noneVid,
-			0,
-			0,
-			this.state.videoWidth,
-			this.state.videoHeight
-		);
+	const grabScreenshot = () => {
+		let ctx = canvas.getContext('2d');
+		ctx.drawImage(noneVid, 0, 0, state.videoWidth, state.videoHeight);
 		var img = new Image();
-		img.src = this.canvas.toDataURL('image/png');
-		this.setState({
-			imgSrc: this.canvas.toDataURL('image/png'),
-			modal: '',
+		img.src = canvas.toDataURL('image/png');
+		dispatch({
+			type: 'UPDATE_STATE',
+			payload: {
+				imgSrc: canvas.toDataURL('image/png'),
+				modal: '',
+			},
 		});
-		img.width = this.state.videoWidth;
+		img.width = state.videoWidth;
 	};
 
-	onHotspotOver = percent => {
-		const currentTime = this.state.duration * (percent / 100);
-		this.noneVid.currentTime = currentTime;
-
-		this.setState({
-			modalLocation: percent,
-			hotspotDuration: fancyTimeFormat(currentTime).split('.')[0],
+	const onHotspotOver = percent => {
+		const currentTime = state.duration * (percent / 100);
+		noneVid.currentTime = currentTime;
+		dispatch({
+			type: 'UPDATE_STATE',
+			payload: {
+				modalLocation: percent,
+				hotspotDuration: fancyTimeFormat(currentTime).split('.')[0],
+			},
 		});
 	};
 
-	onHotspotLeave = () => {
-		this.setState({
-			modal: 'None',
+	const onHotspotLeave = () => {
+		dispatch({
+			type: 'UPDATE_STATE',
+			payload: {
+				modal: 'None',
+			},
 		});
 	};
 
-	Play = () => {
-		if (this.state.currentplayer === 'vid1') this.vid1.play();
+	const Play = () => {
+		if (state.currentplayer === 'vid1') vid1.play();
 
-		if (this.state.currentplayer === 'vid2') this.vid2.play();
+		if (state.currentplayer === 'vid2') vid2.play();
 	};
 
-	Pause = () => {
-		if (this.state.currentplayer === 'vid1') this.vid1.pause();
+	const Pause = () => {
+		if (state.currentplayer === 'vid1') vid1.pause();
 
-		if (this.state.currentplayer === 'vid2') this.vid2.pause();
+		if (state.currentplayer === 'vid2') vid2.pause();
 	};
 
-	render() {
-		const { props } = this;
-		return (
-			<div className="appChild">
-				<div className="VID">
-					<video
-						className="video1"
-						ref={el => (this.vid1 = el)}
-						onSeeked={this.grabScreenshot}
-						onTimeUpdate={this.onTimeUpdate}
-						onPlay={() => this.setState({ playing: true })}
-						onPause={() => this.setState({ playing: false })}
-					>
-						<source src={props.src} type={props.type} />
-						Your browser does not support the video tag.
-					</video>
-					<video
-						className={`video2 ${this.state.visibility}`}
-						name="media"
-						ref={el => (this.vid2 = el)}
-						onTimeUpdate={this.onTimeUpdate}
-						onSeeked={this.grabScreenshot}
-						onPlay={() => this.setState({ playing: true })}
-						onPause={() => this.setState({ playing: false })}
-					>
-						<source src={props.src} type={props.type} />
-						Your browser does not support the video tag.
-					</video>
-				</div>
-				<div className="storyLine">
-					<span
-						className={`modal ${this.state.modal}`}
-						style={{ left: `${this.state.modalLocation}%` }}
-					>
-						<img
-							src={this.state.imgSrc}
-							className="imgSource"
-							alt="Hotspot"
-						/>
-						<p>Time: {this.state.hotspotDuration} Minutes</p>
-					</span>
-					<span
-						className="progressColor"
-						style={{ width: `${this.state.progress}%` }}
-					/>
-					<span
-						className="progress"
-						style={{ left: `${this.state.progress}%` }}
-					/>
-					<Hotspot
-						left={10}
-						onClick={n => this.onHotspotClick(n)}
-						onMouseOver={n => this.onHotspotOver(n)}
-						onMouseLeave={n => this.onHotspotLeave(n)}
-						hotspotDuration={this.state.hotspotDuration}
-					/>
-					<Hotspot
-						left={30}
-						onClick={n => this.onHotspotClick(n)}
-						onMouseOver={n => this.onHotspotOver(n)}
-						onMouseLeave={n => this.onHotspotLeave(n)}
-						hotspotDuration={this.state.hotspotDuration}
-					/>
-					<Hotspot
-						left={50}
-						onClick={n => this.onHotspotClick(n)}
-						onMouseOver={n => this.onHotspotOver(n)}
-						onMouseLeave={n => this.onHotspotLeave(n)}
-						hotspotDuration={this.state.hotspotDuration}
-					/>
-					<Hotspot
-						left={75}
-						onClick={n => this.onHotspotClick(n)}
-						onMouseOver={n => this.onHotspotOver(n)}
-						onMouseLeave={n => this.onHotspotLeave(n)}
-						hotspotDuration={this.state.hotspotDuration}
-					/>
-					<Hotspot
-						left={90}
-						onClick={n => this.onHotspotClick(n)}
-						onMouseOver={n => this.onHotspotOver(n)}
-						onMouseLeave={n => this.onHotspotLeave(n)}
-						hotspotDuration={this.state.hotspotDuration}
-					/>
-				</div>
-				<div>
-					<button className="actionButton" onClick={this.Play}>
-						Play
-					</button>
-					<button className="actionButton" onClick={this.Pause}>
-						Pause
-					</button>
-				</div>
-				<canvas
-					width="1120"
-					height="720"
-					ref={el => (this.canvas = el)}
-					className="canvas None"
-				/>
+	return (
+		<div className="appChild">
+			<div className="VID">
 				<video
-					className="None"
-					ref={el => (this.noneVid = el)}
-					onLoadedMetadata={this.onLoadedMetadata}
-					onTimeUpdate={this.onTimeUpdateForScreenshot}
-					controls
+					className="video1"
+					ref={el => (vid1 = el)}
+					onSeeked={grabScreenshot}
+					onTimeUpdate={onTimeUpdate}
+					onPlay={() =>
+						dispatch({
+							type: 'UPDATE_STATE',
+							payload: { playing: true },
+						})
+					}
+					onPause={() =>
+						dispatch({
+							type: 'UPDATE_STATE',
+							payload: { playing: false },
+						})
+					}
+				>
+					<source src={props.src} type={props.type} />
+					Your browser does not support the video tag.
+				</video>
+				<video
+					className={`video2 ${state.visibility}`}
+					name="media"
+					ref={el => (vid2 = el)}
+					onTimeUpdate={onTimeUpdate}
+					onSeeked={grabScreenshot}
+					onPlay={() =>
+						dispatch({
+							type: 'UPDATE_STATE',
+							payload: { playing: true },
+						})
+					}
+					onPause={() =>
+						dispatch({
+							type: 'UPDATE_STATE',
+							payload: { playing: false },
+						})
+					}
 				>
 					<source src={props.src} type={props.type} />
 					Your browser does not support the video tag.
 				</video>
 			</div>
-		);
-	}
-}
+			<div className="storyLine">
+				<span
+					className={`modal ${state.modal}`}
+					style={{ left: `${state.modalLocation}%` }}
+				>
+					<img
+						src={state.imgSrc}
+						className="imgSource"
+						alt="Hotspot"
+					/>
+					<p>Time: {state.hotspotDuration} Minutes</p>
+				</span>
+				<span
+					className="progressColor"
+					style={{ width: `${state.progress}%` }}
+				/>
+				<span
+					className="progress"
+					style={{ left: `${state.progress}%` }}
+				/>
+				<Hotspot
+					left={10}
+					onClick={n => onHotspotClick(n)}
+					onMouseOver={n => onHotspotOver(n)}
+					onMouseLeave={n => onHotspotLeave(n)}
+					hotspotDuration={state.hotspotDuration}
+				/>
+				<Hotspot
+					left={30}
+					onClick={n => onHotspotClick(n)}
+					onMouseOver={n => onHotspotOver(n)}
+					onMouseLeave={n => onHotspotLeave(n)}
+					hotspotDuration={state.hotspotDuration}
+				/>
+				<Hotspot
+					left={50}
+					onClick={n => onHotspotClick(n)}
+					onMouseOver={n => onHotspotOver(n)}
+					onMouseLeave={n => onHotspotLeave(n)}
+					hotspotDuration={state.hotspotDuration}
+				/>
+				<Hotspot
+					left={75}
+					onClick={n => onHotspotClick(n)}
+					onMouseOver={n => onHotspotOver(n)}
+					onMouseLeave={n => onHotspotLeave(n)}
+					hotspotDuration={state.hotspotDuration}
+				/>
+				<Hotspot
+					left={90}
+					onClick={n => onHotspotClick(n)}
+					onMouseOver={n => onHotspotOver(n)}
+					onMouseLeave={n => onHotspotLeave(n)}
+					hotspotDuration={state.hotspotDuration}
+				/>
+			</div>
+			<div>
+				<button className="actionButton" onClick={Play}>
+					Play
+				</button>
+				<button className="actionButton" onClick={Pause}>
+					Pause
+				</button>
+			</div>
+			<canvas
+				width="1120"
+				height="720"
+				ref={el => (canvas = el)}
+				className="canvas None"
+			/>
+			<video
+				className="None"
+				ref={el => (noneVid = el)}
+				onLoadedMetadata={onLoadedMetadata}
+				onTimeUpdate={onTimeUpdateForScreenshot}
+				controls
+			>
+				<source src={props.src} type={props.type} />
+				Your browser does not support the video tag.
+			</video>
+		</div>
+	);
+};
 export default Video;
